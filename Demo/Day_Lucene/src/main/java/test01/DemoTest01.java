15461d5f4930701a3c01e2394829a2865c3e8622
hujianxiong <1531961255@qq.com>
2018-09-05 21:28:49 +0800

    23342

--- Demo/Day_Lucene/src/main/java/test01/DemoTest01.java
+++ Demo/Day_Lucene/src/main/java/test01/DemoTest01.java
@@ -108,7 +108,7 @@ public class DemoTest01 {
 		CharTermAttribute addAttribute = tokenStream.addAttribute(CharTermAttribute.class);
 		while (tokenStream.incrementToken()) {
 			 System.out.println(addAttribute);
-			 System.out.println(1111);
+			 System.out.println(1111223);
 		}
 	}
 	
